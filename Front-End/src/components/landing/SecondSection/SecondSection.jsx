590c9c6fad046fc01eae9679dfe53cb4ce0648f9
salvareschilisandro <salvareschilisandro0@gmail.com>
2026-06-22 00:12:58 -0300

    Se ajustó los steps en la landing

--- Front-End/src/components/landing/SecondSection/SecondSection.jsx
+++ Front-End/src/components/landing/SecondSection/SecondSection.jsx
@@ -1,6 +1,30 @@
+import step1Image from '../Images/landing/steps/step-1.jpg';
+import step2Image from '../Images/landing/steps/step-2.png';
+import step3Image from '../Images/landing/steps/step-3.jpg';
 import '../FirstSection/FirstSection.css';
-import { steps } from '../../../data/steps';
+
 import { Container, Stack, Row, Col } from 'react-bootstrap';
+
+const steps = [
+    {
+        id: 1,
+        title: "Práctico y fácil de entender",
+        subtitle: "Aprendé con ejemplos reales como compras, descuentos, precios y cuentas del día a día.",
+        src: step1Image
+    },
+    {
+        id: 2,
+        title: "Explicado paso a paso",
+        subtitle: "Lecciones claras y simples para ganar confianza resolviendo problemas cotidianos.",
+        src: step2Image
+    },
+    {
+        id: 3,
+        title: "Pensado para adultos",
+        subtitle: "Una experiencia accesible y enfocada en aprender desde situaciones reales. Sin apuros ni presión. ¡A tu ritmo!",
+        src: step3Image
+    }
+];
 
 const SecondSection = () => {
     return (
