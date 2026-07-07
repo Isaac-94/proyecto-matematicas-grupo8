e3c008289d4fce01dcff0f563e409df741398fff
Romina_Ruiz <romina.ruiz@alumnos.frgp.utn.edu.ar>
2026-07-06 23:31:46 -0300

    Desarrollo componente ejercicio con input

---
+++ Front-End/src/pages/Ejercicios.jsx
@@ -0,0 +1,40 @@
+
+import React, { useState } from 'react';
+import EjercicioPage from '../components/layouts/Ejercicios/Ejercicio'; // Tu componente de ejercicios
+import { ejerciciosDePrueba } from '../components/layouts/Ejercicios/datosPrueba'; // Importamos el array de prueba
+
+function ModuloEjercicios() {
+  const [indexActual, setIndexActual] = useState(0);
+
+  // Obtenemos de forma dinámica el ejercicio actual del array
+  const ejercicioActual = ejerciciosDePrueba[indexActual];
+
+  const manejarAtras = () => {
+    if (indexActual > 0) {
+      setIndexActual(indexActual - 1);
+    } else {
+      console.log("Inicio de la sección de ejercicios.");
+    }
+  };
+
+  const manejarContinuar = () => {
+    if (indexActual < ejerciciosDePrueba.length - 1) {
+      setIndexActual(indexActual + 1);
+    } else {
+      alert("🎉 ¡Felicidades! Has completado todos los ejercicios de prueba.");
+    }
+  };
+
+  return (
+    <EjercicioPage
+      pregunta={ejercicioActual.pregunta}
+      imagenUrl={ejercicioActual.imagenUrl}
+      respuestaCorrecta={ejercicioActual.respuestaCorrecta}
+      onBack={manejarAtras}
+      onContinue={manejarContinuar}
+    />
+  );
+}
+
+export default ModuloEjercicios;
+
