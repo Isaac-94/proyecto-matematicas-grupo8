df77a3155c4ee9d5b9e35c3d622facd8867be3c8
sol <solpeloc@gmail.com>
2026-07-07 18:05:44 -0300

    Cambio diseño header desafio

--- Front-End/src/components/layouts/Desafios/headerDesafio/HeaderDesafio.jsx
+++ Front-End/src/components/layouts/Desafios/headerDesafio/HeaderDesafio.jsx
@@ -1,33 +1,58 @@
 import { Navbar, Container, ProgressBar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import "./headerDesafio.css";
-import calcIcon from "../../../../Images/icons/calculate.png";
-import bookIcon from "../../../../Images/icons/book.png";
-import lightIcon from "../../../../Images/icons/lightbulb_2.png";
+
 export default function HeaderDesafio({ progreso = 50 }) {
   const navigate = useNavigate();
 
   return (
     <div className="header-desafio">
-      <div className="header-desafio-top">
-        <div className="header-desafio-back" onClick={() => navigate(-1)}>
-          <FaArrowLeft />
-          <span>Regresar</span>
+      <div className="header-desafio-progress">
+        <Link to="/" className="header-desafio-back">
+          <img src="/login/iconButton.png" alt="volver" className="back-icon" />
+        </Link>
+
+        <div className="progress-bar-container">
+          <div
+            className="progress-bar-fill"
+            style={{ width: `${progreso}%` }}
+          ></div>
         </div>
-       <div className="header-desafio-icons">
-        <button className="icon-btn"><img src={bookIcon} alt="book" /></button>
-        <button className="icon-btn"><img src={calcIcon} alt="calculadora" /></button>
-        <button className="icon-btn"><img src={lightIcon} alt="foco" /></button>
-       </div>
       </div>
-      <div className="progress-bar-container">
-        <div
-          className="progress-bar-fill"
-          style={{ width: `${progreso}%` }}
-        ></div>
+
+      <div className="header-desafio-icons">
+        <button className="icon-btn">
+          <img src="/icons/Book.png"  alt="book" />
+        </button>
+
+        <button className="icon-btn">
+          <img src="/icons/Calculator.png"alt="calculadora" />
+        </button>
+
+        <button className="icon-btn">
+          <img src="/icons/Light.png"alt="foco" />
+        </button>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
