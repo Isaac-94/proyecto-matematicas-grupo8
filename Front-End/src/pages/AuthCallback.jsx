ed7aca0e733f94b3aa8a320e39aa3a4ab71bca2e
salvareschilisandro <salvareschilisandro0@gmail.com>
2026-07-07 21:22:30 -0300

    Se solucionó import de SupabaseClient

--- Front-End/src/pages/AuthCallback.jsx
+++ Front-End/src/pages/AuthCallback.jsx
@@ -1,7 +1,7 @@
 // pages/auth/Callback.jsx
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../../config/supabaseClient';
+import { supabase } from '../config/supabaseClient';
 import { Container, Spinner, Alert } from 'react-bootstrap';
 
 const AuthCallback = () => {
